2f459a2e5193bcc6f5f63e5d0eabab001a1f466a
Jayin Ton <tonjayin@gmail.com>
2016-01-19 14:51:38 +0800

    move to `feeds`

--- build.js
+++ build.js
@@ -2,22 +2,23 @@
 
 const fs = require('fs')
 const path = require('path')
+const BASE = path.join(__dirname, 'feeds')
 
-let files = fs.readdirSync(__dirname)
+let files = fs.readdirSync(BASE)
 let all = []
 
 files.forEach(function (file, index) {
   if (file.indexOf('.') == 0) {
     return
   }
-  if (fs.lstatSync(path.join(__dirname, file)).isDirectory()) {
+  if (fs.lstatSync(path.join(BASE, file)).isDirectory()) {
     all.push(
-      require(path.join(__dirname, file, 'info.json'))
+      require(path.join(BASE, file, 'info.json'))
     )
   }
 })
 
-fs.writeFileSync(path.join(__dirname, 'all.json'), JSON.stringify(all, null, 4), {encoding: 'utf-8'})
-fs.writeFileSync(path.join(__dirname, 'all.min.json'), JSON.stringify(all, null, 0), {encoding: 'utf-8'})
+fs.writeFileSync(path.join(BASE, 'all.json'), JSON.stringify(all, null, 4), {encoding: 'utf-8'})
+fs.writeFileSync(path.join(BASE, 'all.min.json'), JSON.stringify(all, null, 0), {encoding: 'utf-8'})
 
 console.log('build finish')
